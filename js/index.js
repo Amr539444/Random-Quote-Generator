3976bca68b7d8b938fcdcaa230db3e0afd07831e
Amr539444 <amre38606@gmail.com>
2024-11-12 00:35:27 +0200

    sec commit4

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
-function clicked() {
-  let quotes = [
-    "“Be yourself; everyone else is already taken.”",
-    "“Be yourself; everyone else is already taken.”",
-    "“So many books, so little time.”",
-    "“A room without books is like a body without a soul.”",
-    "“You only live once, but if you do it right, once is enough.”",
-    "“If you tell the truth, you don`t have to remember anything”",
-  ];
-  let quotes_random = quotes[Math.floor(Math.random() * quotes.length)];
-  document.getElementById("output").innerHTML = quotes_random;
+
+const onClick = [
+  {'author': `― Allen Saunders`, 
+   'quote': `“Life is what happens to us while we are making other plans.”`
+  },
+  {'author': `― Thomas A. Edison`, 
+   'quote': `“I have not failed. Ive just found 10,000 ways that wont work.”`
+  },
+  {'author': `― Mark Twain`, 
+   'quote': `“The man who does not read has no advantage over the man who cannot read.”`
+  },
+  {'author': `― Eleanor Roosevelt`, 
+   'quote': `“A woman is like a tea bag; you never know how strong it is until it's in hot water.”`
+  },
+  {'author': `― Dr. Seuss`, 
+   'quote': `“I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living.”`
+  },
+  {'author': `― Oscar Wilde`, 
+   'quote': `“Be yourself; everyone else is already taken.”`
+  },
+];
+
+function clicked(){
+  const random = Number.parseInt(Math.random()*onClick.length + 1);
+  document.querySelector('#output').textContent = `\"${onClick[random].quote}\"`;
+  document.querySelector('#output2').textContent = `--${onClick[random].author}`;
+  
 }
